e96825ee78b3ddeeafa33b39ba61fdad051a8d2a
furutajp <furutajp@gmail.com>
2016-02-03 19:37:43 -0200

    Add spring-boot-maven-plugin.

--- src/main/java/com/furuta/config/Application.java
+++ src/main/java/com/furuta/config/Application.java
@@ -4,13 +4,15 @@ import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     @ResponseBody
@@ -18,7 +20,12 @@ public class Application {
         return "The time now is " + new Date();
     }
 
-    public static void main(String[] args) throws Exception {
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
