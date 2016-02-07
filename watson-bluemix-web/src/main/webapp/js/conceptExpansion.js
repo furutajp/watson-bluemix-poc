d3a118ba7d752e08656f538eaea85b156ff0984c
furutajp <furutajp@gmail.com>
2016-02-07 20:33:13 -0200

    Improve view.

--- watson-bluemix-web/src/main/webapp/js/conceptExpansion.js
+++ watson-bluemix-web/src/main/webapp/js/conceptExpansion.js
@@ -2,10 +2,22 @@ var app = angular.module('conceptExpansionModule', []);
 
 app.controller('conceptExpansionCtrl', function($scope, $http) {
 	
+	$scope.loading = false;
+	
+	$scope.seeds = 'banana\norange\nstrawberry';
+	
 	$scope.submit = function() {
+		
+		if (!$scope.conceptExpansionForm.$valid) {
+			return;
+		}
+		
+		$scope.loading = true;
+		
 		$http.post("/evaluate", $scope.seeds)
 	     	 .then(function(response) {
 	     		 		$scope.concepts = response.data;
+	     		 		$scope.loading = false;
 	     	 	   });
     };
 });
