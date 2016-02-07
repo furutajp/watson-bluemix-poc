ad8091fb9f79aaf8e841c713d1447fa61a106fb6
furutajp <furutajp@gmail.com>
2016-02-06 23:38:16 -0200

    Add repository.

---
+++ watson-bluemix-domain/src/main/java/com/furuta/repository/ConceptExpansionRepository.java
@@ -0,0 +1,49 @@
+package com.furuta.repository;
+
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Repository;
+
+import com.cloudant.client.api.ClientBuilder;
+import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@Repository
+public class ConceptExpansionRepository {
+
+	private Database database;
+	
+	@PostConstruct
+	private void init() {
+		final CloudantClient client = getCloudantClient();
+		this.database = client.database("concept-expansion", false);
+	}
+
+	public void save(final Map<String, Object> data) {
+		database.save(data);
+	}
+
+	private CloudantClient getCloudantClient() {
+		final JsonObject credentials = getCredentials();
+		final String username = credentials.get("username").getAsString();
+		final String password = credentials.get("password").getAsString();
+		return ClientBuilder.account(username)
+							.username(username)
+							.password(password)
+							.build();
+	}
+	
+	private JsonObject getCredentials() {
+		final String VCAP_SERVICES = System.getenv("VCAP_SERVICES");
+		final JsonObject vcapServices = (JsonObject) new JsonParser().parse(VCAP_SERVICES);
+		final JsonElement cloudantElement = vcapServices.get("cloudantNoSQLDB");
+		final JsonObject service = (JsonObject) ((JsonArray) cloudantElement).get(0);
+		return (JsonObject) service.get("credentials");
+	}
+}
--- watson-bluemix-web/src/main/java/com/furuta/controller/ConceptExpansionController.java
+++ watson-bluemix-web/src/main/java/com/furuta/controller/ConceptExpansionController.java
@@ -1,6 +1,9 @@
 package com.furuta.controller;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.furuta.repository.ConceptExpansionRepository;
 import com.furuta.service.ConceptExpansionService;
 import com.google.gson.Gson;
 import com.ibm.watson.developer_cloud.concept_expansion.v1.model.Concept;
@@ -19,11 +23,24 @@ public class ConceptExpansionController {
 	@Autowired
 	private ConceptExpansionService service;
 	
+	@Autowired
+	private ConceptExpansionRepository repository;
+	
     @RequestMapping(value = "evaluate", method = RequestMethod.POST)
     @ResponseBody
     String evaluate(@RequestBody String seeds) {
     	final String jobId = service.createJob(seeds.split("\n"));
     	final List<Concept> concepts = service.getJobResult(jobId);
-    	return new Gson().toJson(concepts);
+    	final String result = new Gson().toJson(concepts);
+    	
+    	final Map<String, Object> request = new HashMap<String, Object>();
+    	request.put("id", jobId);
+    	request.put("seeds", seeds.split("\n"));
+    	request.put("result", result);
+    	request.put("timestamp", new Date());
+    	
+    	repository.save(request);
+    	
+		return result;
     }
 }
